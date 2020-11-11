8370fcfa5e35b3d4e8513973368aa140a2af9df6
BaileyVillipart1 <Bailey.Villipart1@marist.edu>
2020-11-11 01:20:24 -0500

    Lab 6

--- SemesterProject/hello.py
+++ SemesterProject/hello.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
