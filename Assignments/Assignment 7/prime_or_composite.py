4e5692e584ffc951510b9c0ca57fbd7b0b6e4b2f
BaileyVillipart1 <Bailey.Villipart1@marist.edu>
2020-11-10 23:53:44 -0500

    prime_or_composite update
    There was an issue where it would randomly say "10 is prime" and "10 is composite", but that's it. I tried to fix it a few ways and wound up implementing something similar to what you showed in class. import math didnt work however, so I tried making my own sqrt function which didn't work, so I learning about "from math import ceil/sqrt"

--- Assignments/Assignment 7/prime_or_composite.py
+++ Assignments/Assignment 7/prime_or_composite.py
@@ -1,17 +1,27 @@
+from math import ceil
+from math import sqrt
+
 def prime_or_composite(number):
     
     primeNum = True
     
     if number > 1:
-        for i in range(2, number):
-            if (number % i == 0):
+        for i in range(2, math.ceil(sqrt(number))):
+            if number % i == 0:
                 primeNum = False
+                break
+
+            else:
+                primeNum = True
                 
         if primeNum:
            print (number, "is prime.")
+
+        elif number <= 1:
+            primeNum = False
     
     pass
-
+    
 if __name__ == "__main__":
     numbers = [1, 2, 10, 31, 47, 89, 101, 103, 97, 187, 981, 19201]
     # If you want to test the efficency of your algorithm add this number to the array above -7
