cf509de08449150139d13a2f8c157f1e2b3139fa
BaileyVillipart1 <Bailey.Villipart1@marist.edu>
2020-11-18 00:32:54 -0500

    Assignment 9 Push

---
+++ Assignments/Assignment 9/Assignment 9.py
@@ -0,0 +1,11 @@
+from Calculator import Calculator
+
+compute = Calculator()
+
+print(compute.addition(10, 25))
+print(compute.subtraction(5, 3))
+print(compute.multiplication(5, 10))
+print(compute.division(10, 2))
+print(compute.exponent(5, 3))
+print(compute.square_root(6))
+print(compute.negate(200))
